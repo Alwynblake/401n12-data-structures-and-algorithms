e4618e77406c38732dceb937215b08f9b7a3259d
Alistair Blake <consultations@me.com>
2019-03-31 12:20:02 -0700

    added ll_merge method and testing that it zips together two linkedLists

--- ll_merge/index.js
+++ ll_merge/index.js
@@ -1,6 +1,46 @@
 'use strict';
 
-// const Node = require('./lib/node');
-//
-// //  ***************
-// class  mergeLists ( n1, n2) {
+const Node = require('./lib/node');
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(value) {
+
+    if (!this.head) {
+      this.head = new Node(value);
+      return;
+    }
+
+    let current = this.head;
+    // while there is a next value set that value of current to current.next
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = new Node(value);
+  }
+
+  mergeLists(linkedList1, linkedList2) {
+
+    let current1 = linkedList1.head;
+    let current2 = linkedList2.head;
+    let linkedList3 = new LinkedList();
+
+    while (current1 || current2) {
+      if (current1) {
+        linkedList3.append(current1.value);
+        current1 = current1.next;
+      }
+
+      if (current2) {
+        linkedList3.append(current2.value);
+        current2 = current2.next;
+      }
+    }
+    return linkedList3.head;
+  }
+}
+
+module.exports = LinkedList;

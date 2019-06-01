93da8a4b51a507fadf3eedaf3ea35094d1bf9a90
Alistair Blake <consultations@me.com>
2019-06-01 11:09:05 -0700

    findMaxValue in a binary tree

--- FindMaxValue/Index.js
+++ FindMaxValue/Index.js
@@ -1,1 +1,43 @@
 'use strict';
+
+const Node = require('./lib/node');
+
+ function findmaxvalue (tree) {
+  if (!tree.root) {
+    return
+  }
+  let highvalue = tree.root.value;
+  function recurse(node) {
+    if(node.value > highvalue) {
+      highvalue = node.value;
+    }
+    if(node.left) { recurse(node.left)}
+    if(node.right) { recurse(node.right)}
+    }
+    recurse(tree);
+  return highvalue;
+}
+
+module.exports = findmaxvalue;
+
+
+
+
+
+/////////
+// class BST {
+//   constructor(value) {
+//     this.root = new Node(value);
+//     this.count = 1
+//   }
+//
+//   findmaxvalue() {
+//     let currentNode = this.root;
+// // continue to traverse right, until no more children
+//     while (currentNode.right) {
+//       currentNode = currentNode.right
+//     }
+//
+//     return currentNode.value
+//   }
+// }
